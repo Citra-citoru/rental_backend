a0a722d8a4549e0cfdf474ea4bc5b2016918e096
citra-citoru <citra.rahman@happy5.co>
2023-09-26 19:15:35 +0300

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  set_types(
+    owner: 'User::Owner',
+    customer: 'User::Customer'
+  )
+
+  def owner?
+    type == 'User::Owner'
+  end
+
+  class Owner < User
+  end
+
+  class Customer < User
+  end
 end
