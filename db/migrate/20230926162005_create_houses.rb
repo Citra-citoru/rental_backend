c217666069faee06aebf286b2faaee7001de5757
citra-citoru <citra.rahman@happy5.co>
2023-09-26 19:20:45 +0300

    add house controller

---
+++ db/migrate/20230926162005_create_houses.rb
@@ -0,0 +1,14 @@
+class CreateHouses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.string :type
+      t.string :location
+      t.string :image
+      t.decimal :number_of_bedroom
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_26_155131) do
+ActiveRecord::Schema.define(version: 2023_09_26_162005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "location"
+    t.string "image"
+    t.decimal "number_of_bedroom"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_houses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +36,5 @@ ActiveRecord::Schema.define(version: 2023_09_26_155131) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "houses", "users"
 end
